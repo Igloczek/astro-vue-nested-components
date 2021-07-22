11059ed8ff31c3a7211be7f6faa56d0eaeb168c8
Bartek Igielski <bartek.igielski@snow.dog>
2021-07-23 01:24:13 +0200

    Example with nested Vue components

---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="counter">
+    <button @click="subtract()">-</button>
+    <pre>{{ count }}</pre>
+    <button @click="add()">+</button>
+  </div>
+  <div class="children">
+    <Children />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+import Children from './Children.vue';
+
+export default {
+  components: {
+    Children
+  },
+  setup() {
+    const count = ref(0);
+    const add = () => (count.value = count.value + 1);
+    const subtract = () => (count.value = count.value - 1);
+    return {
+      count,
+      add,
+      subtract,
+    };
+  },
+};
+</script>
